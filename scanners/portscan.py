e687668734e076700508bf3d2e520115897bf302
Daddy-Myth <Archit.yadav2005@gmail.com>
2025-07-01 22:51:30 +0530

    Updated port scanner logic and timeout handling

--- scanners/portscan.py
+++ scanners/portscan.py
@@ -1,6 +1,6 @@
 import socket
 
-def scan_ports(host, ports=[21, 22, 80, 443, 3306, 8080]):
+def scan_ports(host, ports=[21, 22, 23, 69, 80, 111, 135, 137, 138, 139, 1433, 1900, 3306, 3389, 443, 445, 8080,6660, 6661, 6662, 6663, 6664, 6665, 6666, 6667, 6668, 6669]):
     open_ports = []
     for port in ports:
         try:
